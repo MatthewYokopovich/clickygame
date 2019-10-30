ba82afceba395dc6b26dcb674dd8b9e108e0690a
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-10-30 15:35:41 -0500

    Updates

---
+++ precache-manifest.904859eec6a42d726dabd3d04bf3070f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a81cd04ae844bc9a9d41d96385ef4c5b",
+    "url": "/clickygame/index.html"
+  },
+  {
+    "revision": "ceeced7fab44ed92e395",
+    "url": "/clickygame/static/css/main.ff670521.chunk.css"
+  },
+  {
+    "revision": "f189f686c3cfc58dc824",
+    "url": "/clickygame/static/js/2.bd3a9e65.chunk.js"
+  },
+  {
+    "revision": "ceeced7fab44ed92e395",
+    "url": "/clickygame/static/js/main.d3492841.chunk.js"
+  },
+  {
+    "revision": "b4ed2c23f69c9b03baae",
+    "url": "/clickygame/static/js/runtime-main.f70fee65.js"
+  }
+]);
